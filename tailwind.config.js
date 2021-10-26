fd689077679ef8b543f297577711afd6df6fa3a8
Maxime Ghéraille <maxime.gheraille@hotmail.com>
2021-10-26 18:49:52 +0200

    background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/endless-constellation.svg')",
+        "hero-pattern2": "url('/rose-petals.svg')",
+        "hero-pattern3": "url('/cool-background.png')",
+        "hero-pattern4": "url('/polygon-scatter-haikei.svg')",
+      },
+    },
   },
   variants: {
     extend: {},
