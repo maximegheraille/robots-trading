816f3303860262d965e88fbce8301a69bd4d265d
Maxime Ghéraille <maxime.gheraille@hotmail.com>
2021-10-26 18:50:04 +0200

    app with child component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,17 @@ import "../styles/global.scss";
 import type { AppProps } from "next/app";
 import Navigation from "../components/navigation/Navigation";
 const Child: React.FC = ({ children }) => {
+  //max-w-7xl mx-auto px-4 sm:px-6 lg:px-8
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">{children}</div>
+    <div className="*bg-hero-pattern3 bg-cover bg-opacity-30">{children}</div>
   );
 };
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Navigation />
       <Child>
+        <Navigation />
         <Component {...pageProps} />
       </Child>
     </>
